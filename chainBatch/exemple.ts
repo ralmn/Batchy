18910f5fd70d3f1a42c290be418ff329ad37beb3
Redkill2108 <redkill2108@saphir-studio.net>
2016-09-28 00:14:36 +0200

    Update to TypeScript 2.X

--- chainBatch/exemple.ts
+++ chainBatch/exemple.ts
@@ -1,6 +1,6 @@
 /// <reference path="../batchManager.d.ts" />
 /// <reference path="../config.d.ts" />
-/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 import base from './base';
 
 base("exemple");
